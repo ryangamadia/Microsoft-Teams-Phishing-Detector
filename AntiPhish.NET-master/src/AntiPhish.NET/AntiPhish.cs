73cc7805a33b556053f0af76dd395fc049b16ef0
ryangamadia <rgamadia@gmail.com>
2019-02-28 15:51:06 -0500

    added google safe browsing

---
+++ AntiPhish.NET-master/src/AntiPhish.NET/AntiPhish.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntiPhishNET
+{
+    public static class AntiPhish
+    {
+        private static PhishingChecker _checker = new PhishingChecker();
+
+        public static bool IsNotPhishingSite(string url)
+        {
+            var json = _checker.GetResult(
+                    _checker.CheckUrlAsync(url).Result.Split(':')[1].Trim(new char[] { '\"', '}' }));
+
+            return json;
+        }
+
+      //  static void Main(string[] args)
+        //{
+          ///  Console.WriteLine(IsNotPhishingSite("www.google.com"));
+      //  }
+    }
+}
